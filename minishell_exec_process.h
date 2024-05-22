0a066112f63a0111a26e6f87ce5d4eb7df5ea7d4
jeayoungKim529 <jeay529@gmail.com>
2024-05-22 16:27:18 +0900

    header

--- minishell_exec_process.h
+++ minishell_exec_process.h
@@ -6,7 +6,7 @@
 /*   By: jeakim <jeakim@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/17 16:56:05 by jeakim            #+#    #+#             */
-/*   Updated: 2024/05/20 21:28:26 by jeakim           ###   ########.fr       */
+/*   Updated: 2024/05/22 15:42:35 by jeakim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,19 +22,20 @@ typedef struct s_inout{
 }	t_inout;
 
 typedef struct s_process{
-	t_command	*com_list;
-	char		**com;
-	int			n_com;
-	char		**envp;
-	char		**path;
-	char		**path_x;
-	int			prevfd;
-	pid_t		pid;
-	int			fd[2];
-	t_inout		io;
+	t_token_list	*token_list;
+	char			**cmd;
+	int				n_cmd;
+	// char			**envp;
+	t_envp			*envp;
+	char			**path;
+	char			**path_x;
+	int				prevfd;
+	pid_t			pid;
+	int				fd[2];
+	t_inout			io;
 }	t_process;
 
 //minishell_exec_init.c
-void	execute_command(t_process *prcs, t_command);
+void	execute_command(t_process *prcs, t_command_list *list);
 
 #endif
