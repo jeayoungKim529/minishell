d619db42c16a53636d2fd0eb872da3f49ead35ac
jeayoungKim529 <jeay529@gmail.com>
2024-05-22 13:51:51 +0900

    main

--- minishell.c
+++ minishell.c
@@ -6,33 +6,28 @@
 /*   By: jeakim <jeakim@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/10 17:23:07 by jeakim            #+#    #+#             */
-/*   Updated: 2024/05/17 11:04:10 by jeakim           ###   ########.fr       */
+/*   Updated: 2024/05/17 19:12:16 by jeakim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void ft_error()
-{
-
-}
-
-int main()
+int	main(int argc, char *argv[], char *envp[])
 {
 	t_command_list	list;
-	char *line;
+	t_process		prcs;
+	char			*line;
 
-	signal_func();
-	env_func();
-	while(42)
-	{
-		line = readline("minishell>");
-		if (!line)
-			break;
-		parsing(&list, line);
-		execute_command();
-		clear_data();
-	}
-	
+	// signal_func();
+	envp_func(&prcs, envp);
+	// while (42)
+	// {
+	// 	line = readline("minishell>");
+	// 	if (!line)
+	// 		break ;
+	// 	parsing(&list, line);
+		execute_commands(&prcs, &list);
+	// 	clear_data();
+	// }
 	exit(0);
 }
