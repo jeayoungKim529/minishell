9bb95feb7f6b6e869c11d1279b0aa18fc37957ea
jeayoungKim529 <jeay529@gmail.com>
2024-06-14 15:21:43 +0900

    header

--- minishell_exec.h
+++ minishell_exec.h
@@ -6,7 +6,7 @@
 /*   By: jeakim <jeakim@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/17 12:29:17 by jeakim            #+#    #+#             */
-/*   Updated: 2024/06/14 15:01:10 by jeakim           ###   ########.fr       */
+/*   Updated: 2024/06/14 15:21:02 by jeakim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -56,76 +56,72 @@ typedef struct s_process{
 	t_inout			file;
 }	t_process;
 
-//minishell_exec.c
+void	init_process(t_process *prcs);
 void	ft_error_exec(t_process *prcs, char *s, int n);
 void	ft_error_exec_exit(t_process *prcs, char *s, int n);
-//minishell_exec_free.c
-void	free_envp(t_process *prcs);
-void	free_command(t_process *prcs);
-void	free_path(t_process *prcs);
-void	free_exec_envp(t_process *prcs);
-void	free_second_char(char **s);
-//minishell_exec_init.c
-// void	execute_command(t_process *prcs, t_command_list *list);
-int		init_prcs(t_process *prcs, t_command_list *list, t_command_node	*cur);
-//minishell_exec_split.c
-char	**ft_exec_split(char const *s, char c);
-char	**ft_exec_splitsep(char **arr, const char *str, char c, int num);
-char	**ft_exec_free_split(char **arr, int i);
-size_t	exec_count_num(const char *str, char c);
-size_t	ft_exec_split_len(const char *str, char c);
-//minishell_envp.c
+//minishell_exec.c
+void	execute_multi(t_process *prcs, int i);
+void	execute_single(t_process *prcs, int i);
+//minishell_exec_envp.c
 void	envp_func(t_process *prcs, char *envp[]);
-void	save_pwd(t_process *prcs);
+void	init_status_envp(t_process *prcs);
 char	**init_exec_envp(t_process *prcs);
+void	save_pwd(t_process *prcs);
 //minishell_exec_envp_utils.c
+t_envp	*ft_envpfind(t_envp *env, char *key);
 t_envp	*ft_envpnew(char *key, char *value);
 void	ft_envpadd(t_envp *env, t_envp *new);
 void	ft_envpdel(t_envp *env, char *key);
-t_envp	*ft_envpfind(t_envp *env, char *key);
-//minishell_exec.c
-void	execute_commands(t_process *prcs, t_command_list *list, int i);
-void	execute_single(t_process *prcs, int i);
-void	execute_multi(t_process *prcs, int i);
+//minishell_exec_split.c
+size_t	ft_strlen_split(const char *str, char c);
+size_t	exec_count_num(const char *str, char c);
+char	**ft_exec_split_util(char const *s, char **arr, int i, int len);
+char	**ft_exec_split(char const *s, char c);
+//minishell_exec_init.c
+int		init_prcs(t_process *prcs, t_command_list *list, t_command_node *cur);
+int		init_redirection(t_process *prcs, t_token_list *list);
+void	init_path(t_process *prcs);
 //minishell_exec_merge_command.c
-char	**merge_command(t_process *prcs, t_token_list *cmd_list);
-char	*check_envp(t_process *prcs, t_token_node *node);
+char	**merge_command(t_process *prcs, t_token_list *token_list);
+//minishell_exec_redirection.c
+int		set_redirection(t_process *prcs, t_token_list *list);
+int		set_redirection_read(t_process *prcs, t_token_node *cur);
+int		set_redirection_write(t_process *prcs, t_token_node *cur);
+int		set_single_redirection(t_process *prcs);
+void	set_multi_redirection(t_process *prcs, int i);
 //minishell_exec_builtin.c
 int		execute_builtin(t_process *prcs);
 int		check_builtin_command(char **com);
 int		ft_error_builtin(char *s, int n);
 //minishell_exec_builtin_func1.c
-void	ft_env(t_process *prcs, int flag);
-void	ft_export(t_process *prcs, int i);
 void	ft_unset(t_process *prcs);
+void	ft_export(t_process *prcs, int i);
+void	ft_env(t_process *prcs, int flag);
 //minishell_exec_builtin_func2.c
-void	ft_cd(t_process *prcs);
-void	ft_pwd(t_process *prcs);
-void	ft_echo(t_process *prcs);
 void	ft_exit(t_process *prcs);
-//minishell_exec_builtin_utilst.c
-int		check_option(char *s);
-void	change_pwd(t_process *prcs, char *key, char *value);
-int		check_envp_key(char *s);
-int		ft_isalnum_exit(char *s);
+void	ft_echo(t_process *prcs);
+void	ft_pwd(t_process *prcs);
+void	ft_cd(t_process *prcs);
+//minishell_exec_builtin_utils.c
 int		ft_atoi_exit(char *s);
-// char	*check_last(char *s);
-// char	*change_dir(t_process *prcs, char *s);
-//minishell_exec_redirection.c
-int		set_redirection(t_process *prcs, t_token_list *list);
-int		set_single_redirection(t_process *prcs);
-void	set_multi_redirection(t_process *prcs, int i);
+int		ft_isalnum_exit(char *s);
+int		check_envp_key(char *s);
+void	change_pwd(t_process *prcs, char *key, char *value);
+int		check_option(char *s);
 //minishell_exec_pipex_open.c
-// void	first_command(t_process *prcs);
-// void	last_command(t_process *prcs);
-void	other_command(t_process *prcs, int i);
 void	run_process(t_process *prcs);
+//minishell_exec_pipex_path.c
+char	*check_path(t_process *prcs);
+char	*make_basic_path(t_process *prcs);
 //minishell_exec_pipex_close.c
 void	finish_commands(t_process *prcs, t_command_list *list, int flag);
 void	ft_unlink(t_process *prcs, t_command_list *list);
 void	execute_wait(t_process *prcs, t_command_list *list, int flag);
-//minishell_exec_pipex_path.c
-char	*check_path(t_process *prcs);
-char	*make_basic_path(t_process *prcs);
+//minishell_exec_free.c
+void	free_second_char(char **s);
+void	free_exec_envp(t_process *prcs);
+void	free_path(t_process *prcs);
+void	free_command(t_process *prcs);
+void	free_envp(t_process *prcs);
 
 #endif
