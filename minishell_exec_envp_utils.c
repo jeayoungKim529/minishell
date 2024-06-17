5d6501680a6f952b2ecfea2f9edda56daf91ef36
jeayoungKim529 <jeay529@gmail.com>
2024-06-17 11:12:44 +0900

    exec

--- minishell_exec_envp_utils.c
+++ minishell_exec_envp_utils.c
@@ -3,29 +3,29 @@
 /*                                                        :::      ::::::::   */
 /*   minishell_exec_envp_utils.c                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jimchoi <jimchoi@student.42seoul.kr>       +#+  +:+       +#+        */
+/*   By: jeakim <jeakim@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/03 16:17:36 by jeakim            #+#    #+#             */
-/*   Updated: 2024/06/17 10:32:17 by jimchoi          ###   ########.fr       */
+/*   Updated: 2024/06/17 11:12:25 by jeakim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 #include "minishell_exec.h"
 
-void	print_envp(t_process *prcs)
-{
-	t_envp	*cur;
+// void	print_envp(t_process *prcs)
+// {
+// 	t_envp	*cur;
 
-	printf("envp\n");
-	cur = prcs->envp;
-	printf("%s=%s\n", cur->key, cur->value);
-	while (cur)
-	{
-		printf("%s=%s\n", cur->key, cur->value);
-		cur = cur->next;
-	}
-}
+// 	printf("envp\n");
+// 	cur = prcs->envp;
+// 	printf("%s=%s\n", cur->key, cur->value);
+// 	while (cur)
+// 	{
+// 		printf("%s=%s\n", cur->key, cur->value);
+// 		cur = cur->next;
+// 	}
+// }
 
 void	ft_envpdel(t_envp *env, char *key)
 {
